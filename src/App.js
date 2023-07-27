b59241fbfebde53c6f75ba184035a7b025d2c08d
Ajjai <ajjaipr8@gmail.com>
2023-07-27 08:11:15 +0530

    mades changes to app.js file in dev

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welome to React<code>src/App.js</code> 
+          Welome to dev React <code>src/App.js</code> 
         </p>
         <a
           className="App-link"
